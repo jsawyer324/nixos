0d530f8abac4dcd3fc285804d13d9b97beaf370b
James Sawyer <jsawyer324@gmail.com>
2026-04-05 12:56:08 -0500

    error

--- home.nix
+++ home.nix
@@ -6,10 +6,10 @@
   
   programs.git = {
     enable = true;
-    config = {
-      user.name = "James Sawyer";
-      user.email = "jsawyer324@gmail.com";
-    };
+    # config = {
+    #   user.name = "James Sawyer";
+    #   user.email = "jsawyer324@gmail.com";
+    # };
   };
   
   programs.bash = {
