6ec6d1927ff26805abe51653d159ebbb0cebdbfa
James Sawyer <jsawyer324@gmail.com>
2026-04-04 22:49:57 -0500

    troubleshoot

--- setup/setup.sh
+++ setup/setup.sh
@@ -28,12 +28,12 @@ sudo nixos-generate-config --root /mnt
 sleep 2
 
 #copy
-sudo rm /mnt/etc/nixos/*
-sudo cp ../configuration.nix /mnt/etc/nixos/configuration.nix
-sudo cp ../flake.nix /mnt/etc/nixos/flake.nix
-sudo cp ../home.nix /mnt/etc/nixos/home.nix
-sudo mkdir -p /mnt/etc/nixos/hosts
-sudo cp ../hosts/qemu-vm.nix /mnt/etc/nixos/hosts/qemu-vm.nix
+#sudo rm /mnt/etc/nixos/*
+#sudo cp ../configuration.nix /mnt/etc/nixos/configuration.nix
+#sudo cp ../flake.nix /mnt/etc/nixos/flake.nix
+#sudo cp ../home.nix /mnt/etc/nixos/home.nix
+#sudo mkdir -p /mnt/etc/nixos/hosts
+#sudo cp ../hosts/qemu-vm.nix /mnt/etc/nixos/hosts/qemu-vm.nix
 
 sleep 2
 
@@ -42,6 +42,8 @@ sleep 2
 
 #install
 cd /mnt || exit
+
+
 #sudo nixos-install --flake /mnt/etc/nixos#testnix01
 #sudo nixos-install --flake /mnt/etc/nixos#testnix01
 
