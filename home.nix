bc8a96a367330d669ed88afb678cc50cb97a0327
James Sawyer <jsawyer324@gmail.com>
2026-04-05 12:55:18 -0500

    reverting changes

--- home.nix
+++ home.nix
@@ -5,8 +5,8 @@
   home.homeDirectory = "/home/james";
   
   programs.git = {
-    #enable = true;
-    extraConfig = {
+    enable = true;
+    config = {
       user.name = "James Sawyer";
       user.email = "jsawyer324@gmail.com";
     };
