4564eaec7cd6a1efd14adf7a2553aeeb11c8c3e3
James Sawyer <jsawyer324@gmail.com>
2026-04-04 21:44:09 -0500

    add more verbose nixpkg url

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS from Scratch";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
