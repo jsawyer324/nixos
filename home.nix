aa52228b6890e7189af14bf5207b595b03907373
James Sawyer <jsawyer324@gmail.com>
2026-04-05 19:15:37 -0500

    yet another massive change

--- home.nix
+++ home.nix
@@ -4,6 +4,10 @@
   home.username = "james";
   home.homeDirectory = "/home/james";
 
+  home.file.".config/i3".source = ./config/i3;
+  home.file.".config/polybar".source = ./config/polybar;
+  home.file.".config/kitty".source = ./config/kitty;
+
   programs.git = {
     enable = true;
   };
